2421c19babd3aa7ffe437898ab2056e2cea6dbcf
Sean coyne <scoyne2@kent.edu>
2017-08-07 14:57:49 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using GetDrinxAdmin.Models;
+using LinqToDB.Mapping;
+
+
+namespace GetDrinxAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // need to bring in: image, full name, role, created date, status, email 
+
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "YourDatabase";
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "SELECT col0,col1 FROM YourTable";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                }
+            }
+
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/UsersTable.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+using LinqToDB.Mapping;
+using MySql.Data.MySqlClient;
+
+namespace GetDrinxAdmin.Models
+{
+    [Table(Name = "Users")]
+    public class User
+    {
+
+        private string _UserID;
+        [Column(IsPrimaryKey = true, Storage = "_UserID")]
+        public string UserID
+        {
+            get
+            {
+                return this._UserID;
+            }
+            set
+            {
+                this._UserID = value;
+            }
+
+        }
+
+        private string _FullName;
+        [Column(Storage = "_FullName")]
+        public string FullName
+        {
+            get
+            {
+                return this._FullName;
+            }
+            set
+            {
+                this._FullName = value;
+            }
+        }
+    }
+
+
+
+    public class DBConnection
+        {
+            private DBConnection()
+            {
+            }
+
+            private string databaseName = string.Empty;
+            public string DatabaseName
+            {
+                get { return databaseName; }
+                set { databaseName = value; }
+            }
+
+            public string Password { get; set; }
+            private MySqlConnection connection = null;
+            public MySqlConnection Connection
+            {
+                get { return connection; }
+            }
+
+            private static DBConnection _instance = null;
+            public static DBConnection Instance()
+            {
+                if (_instance == null)
+                    _instance = new DBConnection();
+                return _instance;
+            }
+
+            public bool IsConnect()
+            {
+                bool result = true;
+                if (Connection == null)
+                {
+                    if (String.IsNullOrEmpty(databaseName))
+                        result = false;
+                    string connstring = string.Format("Server=50.112.69.94; database={0}; UID=getdrinxown_usr; password=your password", databaseName);
+                    connection = new MySqlConnection(connstring);
+                    connection.Open();
+                    result = true;
+                }
+
+                return result;
+            }
+
+            public void Close()
+            {
+                connection.Close();
+            }
+        }
+
+   
+
+    }
+
